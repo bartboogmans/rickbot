2a185828a05af3533eb87a73d6af770b7f5de58e
Bart <bartboogmans@hotmail.com>
2024-04-17 16:14:20 +0200

    Edit project details

--- setup.py
+++ setup.py
@@ -4,22 +4,26 @@ package_name = 'rickbot'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # include srt/Rick-Astley-Never-Gonna-Give-You-Up.srt
+        ('share/' + package_name, ['srt/Rick-Astley-Never-Gonna-Give-You-Up.srt']),
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='bart',
     maintainer_email='bartboogmans@hotmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Streams srt subtitles to specified ros topic',
+    license='MIT License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'rickbotmain = rickbot.rickbotmain:main'
         ],
     },
 )
